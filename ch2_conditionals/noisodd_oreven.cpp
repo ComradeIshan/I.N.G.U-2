8b477cc05755282a71eb4b6f6721ccef71a84af3
Ishan <ishanshahi200@gmail.com>
2026-06-15 17:27:49 +0530

    done with the divisibility checks

--- ch2_conditionals/noisodd_oreven.cpp
+++ ch2_conditionals/noisodd_oreven.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 int main()
 {
-    
+    int n; //Int can take at only certain maximum value, like when i was entering value like 154987974894894 it was displaying somthng randomn, to avoid this 
+    cout << "Enter an integer: "; //we can use long long datatype.
+    cin >> n;
+    if (n % 2 == 0) {
+        cout << n << " is even.\n" ;
+    } else {
+        cout << n << " is odd.\n" ;
+    }
     return 0;
 }
