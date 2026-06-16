14f7352991d7a8ee4f0792cff678036043d8b691
Ishan <ishanshahi200@gmail.com>
2026-06-16 23:26:20 +0530

    done for the day, closing at 11:30pm

---
+++ ch3_LOOPS/forlopp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout <<  "Enter the number: ";
+    cin >> n;
+    for (int i = 1; i <= n ; i++) {
+        cout << i << " Hi ishan\n";
+    }
+    return 0;
+}
