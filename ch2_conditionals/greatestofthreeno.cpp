73341deaef60d9e99f26fc50edbcc7c8ed210464
Ishan <ishanshahi200@gmail.com>
2026-06-16 17:01:53 +0530

    Started again at 5pm

---
+++ ch2_conditionals/greatestofthreeno.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cout << "Enter 1st number: ";
+    cin >> a;
+    cout << "Enter 2nd number: ";
+    cin >> b;
+    cout << "Enter 3rd number: ";
+    cin >> c;
+
+    if (a>b && a>c) {
+        cout << a << " is the biggest number.";
+    } else if (b>a && b>c){
+        cout << b << " is the biggest number.";
+    } else{
+        cout << c << " is the biggest number.";
+    }
+
+    return 0;
+}
