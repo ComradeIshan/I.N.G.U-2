e353030a7d5fcee1bcaec20245255305e54b3045
Ishan <ishanshahi200@gmail.com>
2026-06-21 21:04:19 +0530

    revising

--- ch1_basics/typecasting.cpp
+++ ch1_basics/typecasting.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main () {
     int x;
     cin >> x;
-    float y = (float) x; //typecasting is used to convert one data type into another data type. here we are converting int into float
+    float y = (float) x; //typecasting is used to convert one data type into another data type. Here we are converting int into float
     cout << y/2 << endl;
 
 }
--- ch1_basics/variables.cpp
+++ ch1_basics/variables.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main() {  //x = x + 2 means the same as x += 2 similarly for x -= and x *=
+int main() {  //x = x + 2 means the same as x += 2 similarly for x -=, x *= adn x/=.
     int x, y;
+    
     x = 5; y = 10;
+    x /= 8;         //Think yourself what will happen here.
     int sum = x + y;
     cout << sum;
 } 
