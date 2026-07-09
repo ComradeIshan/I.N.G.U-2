3c03ac108b8db35b001aeaa104c9f66f8f3ab600
Ishan <ishanshahi200@gmail.com>
2026-07-10 00:10:52 +0530

    Added a more optimised approach of the gp question

--- ch3_LOOPS/gp.cpp
+++ ch3_LOOPS/gp.cpp
@@ -4,7 +4,12 @@ using namespace std;
 int main () {
     int a = 99;
     int n;
-    for (int i = 1; (-4*n + 103)>0; i++) {
-        cout << a;
-    } 
+    // for (int i = 1; i<=25; i++) {
+    //     cout << a << endl;
+    //     a = a - 4;
+    for (int i = 99; i > 0; i-=4) {     //more optimised approach
+        cout << a << " ";
+        a = a-4;
+    }
+     
 }
