519707e405f306c335c3a36719f4b84275e67b41
Ishan <ishanshahi200@gmail.com>
2026-07-11 13:21:53 +0530

    Very nice question in series printing

--- Questions using operators/q10.cpp
+++ Questions using operators/q10.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int main()         //Take 'n' as input from user and print the following sequence -> 1, n, 2, n-1, 3, n-2...
 {
-     
+    int n;
+    cout << "Enter the num: ";
+    cin >> n;
+    for (int i=1; i <= n; i++) {
+        cout << i << endl;
+        cout << n << endl;
+        n-=1;                                   //more good approach -
+                                                //cout << i << endl;
+                                                // cout << n - i + 1 << endl;     here, the input variable is not changing, means it would remain the same, which is a good practise in programming.
+    }
     return 0;
 }
